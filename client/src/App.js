bfbbdbc591b84b5e5a88d32f5820ea73dcf94703
Raghavendra <raghavv2710@gmail.com>
2025-04-18 02:34:59 +0530

    Checking localhost

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
